f48f8f367e02e4329719027789ef94fd586831db
Mathias Cochet <Mathias_cochet@hotmail.com>
2021-03-30 20:08:45 +0200

    null-safety implementation

--- localization_annotation/lib/localization_annotation.dart
+++ localization_annotation/lib/localization_annotation.dart
@@ -3,13 +3,13 @@ library localization_annotation;
 ///generate an AppLocalizations class with [name] based on yaml found at [yamlStringsPath] for [supportedLocals]
 class GenerateAppLocalizationsConfig {
   ///name of the class, e.g 'AppLocalizations'
-  final String name;
+  final String? name;
 
   ///the path where the yaml is located. must be in the lib folder, e.g. 'libs/l10n/strings.yaml'
-  final String yamlStringsPath;
+  final String? yamlStringsPath;
 
   ///list of locals that are supported, e.g ['en', 'nl']
-  final List<String> supportedLocals;
+  final List<String>? supportedLocals;
 
   final SeparatorStyle separatorStyle;
 
